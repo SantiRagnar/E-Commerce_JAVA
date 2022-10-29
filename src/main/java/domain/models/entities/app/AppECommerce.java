0b58bf7eae0cc9fc0812c3e12f248e8e825bfdaf
SantiRagnar <santbarrios130600@gmail.com>
2022-10-29 14:30:29 -0300

    inicio spring

---
+++ src/main/java/domain/models/entities/app/AppECommerce.java
@@ -0,0 +1,12 @@
+package domain.models.entities.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppECommerce {
+    public static void main(String[] args) {
+        SpringApplication.run(AppECommerce.class, args);
+    }
+
+}
